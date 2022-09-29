2d623648c8bc2d7466494b6e7094ddf3fb9c1135
stla <laurent_step@yahoo.fr>
2022-09-29 08:00:20 +0200

    Fisher

--- R/noncentral.R
+++ R/noncentral.R
@@ -42,20 +42,42 @@ qt_boost <- function(p, nu, ncp = 0, lower.tail = TRUE) {
 #' 
 #' @name ChiSquaredDistribution
 pchisq_boost <- function(q, nu, ncp = 0, lower.tail = TRUE) {
+  stopifnot(nu > 0, ncp >= 0)
   rcpp_pchisq(q, nu, ncp, lower.tail)
 }
 
 #' @rdname ChiSquaredDistribution
 #' @export
 qchisq_boost <- function(p, nu, ncp = 0, lower.tail = TRUE) {
+  stopifnot(nu > 0, ncp >= 0)
   rcpp_qchisq(p, nu, ncp, lower.tail)
 }
 
+#' The non-central Fisher distribution
+#' @description Distribution function and quantile function for the non-central 
+#'   Fisher distribution.
+#'
+#' @param q numeric vector of quantiles
+#' @param p numeric vector of probabilities
+#' @param nu1 numerator degrees of freedom parameter, a positive number
+#' @param nu2 denominator degrees of freedom parameter, a positive number
+#' @param ncp non-centrality parameter, a non-negative number
+#' @param lower.tail Boolean, whether to use the complementary probabilities
+#'
+#' @return A numeric vector of probabilities (\code{pf_boost}) or a 
+#'   numeric vector of quantiles (\code{qf_boost}).
+#' @export
+#' 
+#' @name FisherDistribution
 pf_boost <- function(q, nu1, nu2, ncp = 0, lower.tail = TRUE) {
+  stopifnot(nu1 > 0, nu2 > 0, ncp >= 0)
   rcpp_pf(q, nu1, nu2, ncp, lower.tail)
 }
 
+#' @rdname FisherDistribution
+#' @export
 qf_boost <- function(p, nu1, nu2, ncp = 0, lower.tail = TRUE) {
+  stopifnot(nu1 > 0, nu2 > 0, ncp >= 0)
   rcpp_pf(p, nu1, nu2, ncp, lower.tail)
 }
 
